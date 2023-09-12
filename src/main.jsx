c020b94926ef40eb24da8937a820eaa3aab1d888
nmywrld <111645407+nmywrld@users.noreply.github.com>
2023-09-12 13:52:03 +0800

    troubleshoot merge

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ErrorBoundary
       FallbackComponent={Fallback}
     >
-      <Choosemeals />
+      {/* <Choosemeals /> */}
 
     </ErrorBoundary>
   </React.StrictMode>,
