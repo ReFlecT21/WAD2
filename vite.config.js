9ce4f9833f38e407966f842e4665bbafd0b7da1b
nmywrld <111645407+nmywrld@users.noreply.github.com>
2023-09-13 12:33:36 +0800

    update proxy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  proxy: {
+    "/test": "http://localhost:5000/",
+  },
   plugins: [react()],
 })
