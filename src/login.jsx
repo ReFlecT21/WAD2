5d0476b03d0b3fc6147c31dfe352d2d8f4568eec
ReFlecT21 <kumaraguru818@gmail.com>
2023-09-13 17:34:02 +0800

    feat: input login page

---
+++ src/login.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Container, Row, Col, Image, Form, Button } from "react-bootstrap";
+import "./login.css";
+
+const LoginComponent = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className="backgroundLeft d-flex">
+          <Image src="login.png" alt="" className="foodimg" />
+        </Col>
+        <Col className="backgroundRight d-flex">
+          <Row>
+            <Row className="d-flex justify-content-center">
+              <Image src="MenuMate.png" alt="" className="logo" />
+            </Row>
+            <Row className="d-flex justify-content-center">
+              <h2 className="tagline">Your friend in every meal.</h2>
+            </Row>
+            <Row className="d-flex justify-content-center">
+              <Form.Control
+                type="text"
+                placeholder="Email"
+                className="inputBox"
+                id="text-input"
+              />
+            </Row>
+            <Row className="d-flex justify-content-center">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                className="inputBox"
+              />
+            </Row>
+            <Row className="d-flex justify-content-center">
+              <Button type="submit" className="loginButton">
+                LOGIN
+              </Button>
+            </Row>
+            <Row className="d-flex justify-content-center">
+              <p>
+                Do not have an account? <a href="#">Sign up</a>
+              </p>
+            </Row>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default LoginComponent;
--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,19 @@ import ReactDOM from "react-dom/client";
 import NavBar from "./NavBar.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import './index.css'
-import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorBoundary } from "react-error-boundary";
+import LoginComponent from "./login.jsx";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Fallback from './fallback.jsx';
+import Fallback from "./fallback.jsx";
 import Choosemeals from "./ChooseMeals.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NavBar />
-    <ErrorBoundary
-      FallbackComponent={Fallback}
-    >
-      <Choosemeals />
-
+    {/* <NavBar /> */}
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <LoginComponent />
     </ErrorBoundary>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
