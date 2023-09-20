b55b4874a918ff6a96a7dc64e578b825b858a890
nmywrld <111645407+nmywrld@users.noreply.github.com>
2023-09-21 00:15:50 +0800

    nutritionix integration

--- functions/API.js
+++ functions/API.js
@@ -7,8 +7,11 @@ const foodAPI = {
   //   },
   // },
   // apikey: "b379279f538a412dabf422f2f0b13eb7",
-  apikey: axios.defaults.headers["x-api-key"] =
-  "b379279f538a412dabf422f2f0b13eb7",
+  // apikey: axios.defaults.headers["x-api-key"] =
+  // "b379279f538a412dabf422f2f0b13eb7",
+  nutritionix_id: axios.defaults.headers["x-app-id"] =`c77e135d`,
+  nutritionix_api: axios.defaults.headers["x-app-key"] =`df9a1e155a2b2378799693c8349a11fd`,
+  // nutritionix_userID: axios.defaults.headers["x-remote-user-id"] = "0",
   APIRoot: "https://api.spoonacular.com/",
   getOne: async function(
     id,
@@ -61,6 +64,28 @@ const foodAPI = {
       console.error(error);
     }
   },
+  manualSearch: async function(
+    params=null,
+  ) {
+    const url = `https://www.nutritionix.com/track-api/v2/natural/nutrients`;
+    try {
+      const response = await axios.post(url, {
+        query:"prata",
+        include_subrecipe:true,
+        use_raw_foods:false,
+        line_delimited:true,
+        claims:true,
+        taxonomy:true,
+        ingredient_statement:true,
+      }
+      );
+      console.log(response.data);
+      // return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
 };
 
 module.exports = foodAPI;
+
