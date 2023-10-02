715f05b6bce4e8ab4a0cd6bd7a429c41ddfec44a
nmywrld <drago-tan@hotmail.com>
2023-10-03 00:01:48 +0800

    barcode fetch complete

--- functions/barcode.js
+++ functions/barcode.js
@@ -1,15 +1,46 @@
 const {default: axios} = require("axios");
+const Quagga = require('quagga').default;
+const fetch = require('node-fetch'); 
+
+// const barcode ={
+//   processBarcode: function(file){
+
+//   },
+// }
 
 
+class BarcodeScanner {
+  constructor() {}
+  // Method to scan barcodes in a static image
+  async scanBarcode(imagePath) {
+    axios.get(imagePath, { responseType: 'arraybuffer' })
+    .then((response) => {
+        // Create a buffer from the response data
+        const imageBase64 = Buffer.from(response.data).toString('base64');
 
-// Import the necessary modules
-
-const barcode ={
-  processBarcode: function(file){
-
-  },
+        // Decode the barcode using QuaggaJS
+        Quagga.decodeSingle(
+        {
+            src: `data:image/jpeg;base64,${imageBase64}`,
+            numOfWorkers: 0,
+            decoder: {
+            readers: ['ean_reader'], // Adjust the readers as needed
+            },
+        },
+        (result) => {
+            if (result && result.codeResult) {
+                console.log('Barcode detected and decoded: ' + result.codeResult.code);
+                return result.codeResult.code
+            } else {
+                console.log('No barcode detected.');
+            }
+        }
+        );
+    })
+    .catch((error) => {
+      console.error('Error downloading image:', error);
+    });
+  }
 }
 
-
-
-module.exports = barcode;
+module.exports = BarcodeScanner;
--- functions/telebot.js
+++ functions/telebot.js
@@ -1,11 +1,11 @@
 const TelegramBot = require("node-telegram-bot-api");
-const barcode = require("./barcode");
+const BarcodeScanner = require("./barcode.js");
 // const axios = require("axios");
 // https://github.com/hosein2398/node-telegram-bot-api-tutorial
 // MenuMateBot MAIN
-const token = "6579495868:AAGgKnnPilbLVSGR4xKv9V4a8cG-O1FI-lM";
+// const token = "6579495868:AAGgKnnPilbLVSGR4xKv9V4a8cG-O1FI-lM";
 // MenuMateBot TEST
-// const token = "6313612860:AAGENA_lQxKjLFkCrQyJz3F86DGs_H1ZFsI";
+const token = "6313612860:AAGENA_lQxKjLFkCrQyJz3F86DGs_H1ZFsI";
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
@@ -27,61 +27,30 @@ bot.on("message", msg => {
 // });
 
 
-// bot.on('photo', async (msg) => {
-//   const chatId = msg.chat.id;
-//   const photo = msg.photo[msg.photo.length - 2]; // Get the highest resolution version
-
-//   // Get the file_id of the photo
-//   const file_id = photo.file_id;
-
-//   // Use the Telegram API's getFile method to get the file path
-//   const fileInfo = await bot.getFile(file_id);
-
-//   // Build the URL for the photo file
-//   const fileURL = `https://api.telegram.org/file/bot${token}/${fileInfo.file_path}`;
-
-//   // Use Axios or any other library to download the photo
-//   axios({
-//     method: 'get',
-//     url: fileURL,
-//     responseType: 'stream',
-//   })
-//     .then(function (response) {
-//       // Process the downloaded photo here
-//       // const filePath = 'downloaded_photo.jpg';
-//       // const stream = fs.createWriteStream(filePath);
-//       // response.data.pipe(stream);
-//       // stream.on('finish', () => {
-//         // Now, you can work with the downloaded photo in 'downloaded_photo.jpg'
-//         // For example, you can use an image processing library to manipulate the image
-//         console.log(response.data);
-//         // Respond to the user or perform any other action you need.
-//         // bot.sendMessage(chatId, 'Photo received and processed successfully!');
-//         bot.sendPhoto(chatId, response);
-//       // });
-//     })
-//     .catch(function (error) {
-//       console.error('Error downloading photo:', error);
-//     });
-// });
-
 bot.on('photo', async (msg) => {
   const chatId = msg.chat.id;
-  const photo = msg.photo[msg.photo.length - 2]; // Get the highest resolution version
+  const photo = msg.photo[msg.photo.length - 2]; 
 
   // Get the photo file using Telegram's getFile method
   const file = await bot.getFile(photo.file_id);
   const photoUrl = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
 
-  // Define the API endpoint where you want to send the photo
+  const barcodeScanner = new BarcodeScanner();
 
   try {
     // Send the photo to the API using Axios
     // const response = await axios.post(apiUrl, { photo_url: photoUrl });
-    const response = barcode.processBarcode(photoUrl)
+    // const response = barcode.processBarcode(photoUrl)
 
     // If the API successfully processes the photo, you can send a message to the user
-    bot.sendMessage(chatId, `Your photo has been processed successfully! ${response}`);
+    await barcodeScanner.scanBarcode(photoUrl)
+      .then((barcode) => {
+        console.log('Scanned barcode:', barcode);
+        bot.sendMessage(chatId, `Your photo has been processed successfully! ${barcode}`);
+      })
+      .catch((error) => {
+        console.error('Error:', error.message);
+      });
 
   } catch (error) {
     // Handle Axios errors
