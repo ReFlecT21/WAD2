bdf24b3c1e5dc980a08e464f9d49074542fe33c1
CherniseTan <chernisetjx02@gmail.com>
2023-10-06 10:08:18 +0800

    inital scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import "./index.css";
-// import "./index.scss"
+// import "./index.css";
+import "./index.scss";
 // import "./App.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
