59044f21901c226b13080fbe05bb0594bd337098
nmywrld <111645407+nmywrld@users.noreply.github.com>
2023-10-07 14:15:23 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import "./index.css";
-// import "./oldindex.css";
+// import "./index.css";
+import "./oldindex.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
