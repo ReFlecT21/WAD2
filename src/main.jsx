b5ad0126159bb455a53db4f13e2e0eac95ab4dfd
nmywrld <111645407+nmywrld@users.noreply.github.com>
2023-10-07 14:08:15 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-// import "./index.css";
-import "./oldindex.css";
-// import "./App.css"
+import "./index.css";
+// import "./oldindex.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
