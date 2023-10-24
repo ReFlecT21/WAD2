7a66599cddc0fd71f34e7f362bb7f2ed71e75f77
nmywrld <drago-tan@hotmail.com>
2023-10-25 00:03:24 +0800

    clean up imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as CreateMealContent } from "./CreateMealPlanContent";
+// export { default as CreateMealContent } from "./CreateMealPlanContent";
 export { default as NavBar } from "./NavBar";
 export { default as Loader } from "./Loader";
 export { default as CompletedMeals } from "./CompletedMeals";
