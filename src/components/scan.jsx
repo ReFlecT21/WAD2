150ddf3bf9b56ffd0cee649bd1d2cd40adedb9d2
nmywrld <111645407+nmywrld@users.noreply.github.com>
2023-10-31 11:07:55 +0800

    Update scan.jsx

--- src/components/scan.jsx
+++ src/components/scan.jsx
@@ -1,19 +1,41 @@
+import React, { useEffect, useState } from 'react';
 import Tesseract from 'tesseract.js';
 
 export function Scan() {
-  const handleImageUpload = async (event) => {
-    const imageFile = event.target.files[0];
-    if (!imageFile) return;
+    useEffect(() => {
+        const inputElement = document.getElementById("input-file");
+        const inputFile = inputElement; // Create a reference to the file input element
 
-    const { data: { text } } = await Tesseract.recognize(imageFile, 'eng');
-    console.log(text);
-  };
+        inputElement.addEventListener("change", async (event) => {
+            const imageFile = event.target.files[0];
+            if (!imageFile) return;
 
-  return (
-    <div>
-      <input className="homePageBtn" type="file" accept="image/*" onChange={handleImageUpload} />
-    </div>
-  );
+            // Recognize text from the uploaded image
+            const { data: { text } } = await Tesseract.recognize(imageFile, 'eng');
+
+            // Output the recognized text to the console
+            console.log(text);
+            console.log(typeof text);
+        });
+
+        // return () => {
+        //     // Clean up the event listener when the component unmounts
+        //     inputElement.removeEventListener("change", handleImageChange);
+        // };
+    }, []);
+
+    const handleImageUpload = () => {
+        // Trigger the click event on the file input element
+        const inputElement = document.getElementById("input-file");
+        inputElement.click();
+    };
+
+    return (
+        <>
+            <input id="input-file" className="d-none" type="file" />
+            <button onClick={handleImageUpload} className="homePageBtn">
+                Upload
+            </button>
+        </>
+    );
 }
-
-
